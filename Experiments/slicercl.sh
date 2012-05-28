5b048e7e9072b099d818794f72618682964c4114
Steve Pieper <pieper@isomics.com>
2012-05-28 14:26:02 -0400

    ENH: more robust installation script

--- Experiments/slicercl.sh
+++ Experiments/slicercl.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-#SLICER_SUPERBUILD="${HOME}/slicer4/latest/Slicer-superbuild"
-SLICER_SUPERBUILD="${HOME}/slicer4/latest/Slicer4-superbuild"
-SLICER_BUILD="${SLICER_SUPERBUILD}/Slicer-build"
-PYTHONEXE="${SLICER_SUPERBUILD}/python-build/bin/python"
-LAUNCH="${SLICER_BUILD}/Slicer --launcher-no-splash --launch"
-PYTHON="${LAUNCH} ${PYTHONEXE}"
+SLICER_SUPERBUILD="${HOME}/slicer4/latest/Slicer-superbuild"
+if [ -e ${SLICER_SUPERBUILD} ]; then
+  SLICER_BUILD="${SLICER_SUPERBUILD}/Slicer-build"
+  SLICER_BUILD="${SLICER_SUPERBUILD}/Slicer-build"
+  PYTHONEXE="${SLICER_SUPERBUILD}/python-build/bin/python"
+  LAUNCH="${SLICER_BUILD}/Slicer --launcher-no-splash --launch"
+  PYTHON="${LAUNCH} ${PYTHONEXE}"
+fi
 
 OPENCLINSTALL=/usr/local/cuda
 
@@ -24,12 +26,22 @@ git clone https://github.com/pieper/pyopencl.git
 cd pyopencl
 git submodule init
 git submodule update
-${PYTHON} configure.py --cl-enable-gl \
-  #--cl-inc-dir=${OPENCLINSTALL}/include \
-  #--cl-lib-dir=${OPENCLINSTALL}/lib \
-  #--cl-libname=OpenCL \
-  --no-cl-enable-device-fission
+
+if [ `uname` = Darwin ]; then
+  # MAC:
+  ${PYTHON} configure.py --cl-enable-gl \
+    --no-cl-enable-device-fission
+else
+  # LINUX:
+  ${PYTHON} configure.py --cl-enable-gl \
+    --cl-inc-dir=${OPENCLINSTALL}/include \
+    --cl-lib-dir=${OPENCLINSTALL}/lib \
+    --cl-libname=OpenCL \
+    --no-cl-enable-device-fission
+fi
 
 export PATH=${SLICER_SUPERBUILD}/python-build/bin:${PATH}
 
 ${LAUNCH} /usr/bin/make install
+
+echo "Built in ${tmpdir}"
